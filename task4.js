b4b7dda1bdbf7ca5b133feedd82d1746be491095
OlgaMakukha <makukhaolga93@gmail.com>
2024-04-11 18:36:06 +0500

    task4

---
+++ task4.js
@@ -0,0 +1,20 @@
+//Infinity
+console.log(Infinity - "1");
+
+//1984 string
+console.log("19" + 84);
+
+//13 number
+console.log(2 + +"11");
+
+//200 number
+console.log(99 + 101);
+
+//0 
+console.log("1" - "1");
+
+//Result: 5
+console.log("Result: " + 10/2);
+
+//3 bananas 2 apples
+console.log(3 + " bananas " + 2 + " apples ");
